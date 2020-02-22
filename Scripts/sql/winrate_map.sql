195eecaf043523f398ce759e519a31458a25171f
Derick Tseng <derickboss1@gmail.com>
2020-02-21 19:00:59 -0800

    change sql

--- Scripts/sql/winrate_map.sql
+++ Scripts/sql/winrate_map.sql
@@ -1,5 +1,5 @@
 SELECT map, opponentrace, SUM(win)/COUNT(win)*100 AS winrate
 FROM replays
-WHERE (win=1.0 OR win=0.0) AND NOT instr(tags, "unranked") AND NOT instr(tags, "custom")
+WHERE (win=1.0 OR win=0.0)
 GROUP BY map, opponentrace
 ORDER BY map, opponentrace;
