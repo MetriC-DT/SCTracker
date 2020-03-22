664a95475904d2a7c6b4053db32d83707bcf6a5f
Derick Tseng <derickboss1@gmail.com>
2020-03-21 21:53:11 -0700

    Update scripts for this season

--- Scripts/sql/opponent_map.sql
+++ Scripts/sql/opponent_map.sql
@@ -1,4 +1,11 @@
 SELECT map, opponentrace, COUNT(map)
 FROM replays
+WHERE map=="Eternal Empire LE"
+OR map=="Ever Dream LE"
+OR map=="Simulacrum LE"
+OR map=="Nightshade LE"
+OR map=="Purity and Industry LE"
+OR map=="Zen LE"
+OR map=="Golden Wall LE"
 GROUP BY opponentrace, map
 ORDER BY opponentrace, map;
--- Scripts/sql/winrate_map.sql
+++ Scripts/sql/winrate_map.sql
@@ -1,5 +1,12 @@
 SELECT map, opponentrace, SUM(win)/COUNT(win)*100 AS winrate
 FROM replays
-WHERE (win=1.0 OR win=0.0)
+WHERE (win=1.0 OR win=0.0) AND 
+(map=="Eternal Empire LE"
+OR map=="Ever Dream LE"
+OR map=="Simulacrum LE"
+OR map=="Nightshade LE"
+OR map=="Purity and Industry LE"
+OR map=="Zen LE"
+OR map=="Golden Wall LE")
 GROUP BY map, opponentrace
 ORDER BY map, opponentrace;
