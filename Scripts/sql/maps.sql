9dda2da7f2b2d6cf0ef5e5cd2ea57e7fd771555a
Derick Tseng <derickboss1@gmail.com>
2020-03-31 22:42:13 -0700

    fix map script for this season

--- Scripts/sql/maps.sql
+++ Scripts/sql/maps.sql
@@ -1,4 +1,11 @@
 SELECT map
 FROM replays
+WHERE map=="Eternal Empire LE"
+OR map=="Ever Dream LE"
+OR map=="Simulacrum LE"
+OR map=="Nightshade LE"
+OR map=="Purity and Industry LE"
+OR map=="Zen LE"
+OR map=="Golden Wall LE"
 GROUP BY map
 ORDER BY map;
